4578a0df9cc5b95352e1b8368773e1f3667de41e
Niels van Huijstee <niels.vanhuijstee@cyso.com>
2015-05-04 15:32:13 +0200

    test the send_request method rather than build request
    The send request seems to be the public api.

--- tests/tests.py
+++ tests/tests.py
@@ -1,27 +1,24 @@
 from unittest import TestCase
+from mock import patch, Mock
 from nexmomessage.nexmo import NexmoVerificationRequest, NexmoVerificationCheck
 
-
-class VerificationMessageTest(TestCase):
+class NexmoMessagesTest(TestCase):
     def setUp(self):
         self.test_key = 'key12345'
         self.test_secret = 'secret12'
-        self.msg = {
+        self.msg_config = {
             'api_key': self.test_key,
             'api_secret': self.test_secret,
             'text': 'Message Body',
         }
 
-    def test_it_should_build_a_verification_request_message(self):
+    def test_it_should_send_a_verification_request_message(self):
         test_brand = 'TESTBRAND'
-        self.msg['from'] = test_brand
         test_number = 31612345678
-        self.msg['to'] = test_number
-        sms = NexmoVerificationRequest(self.msg)
-
-        request = sms.build_request()
-
-        expected_request = (
+        self.msg_config['from'] = test_brand
+        self.msg_config['to'] = test_number
+        sms = NexmoVerificationRequest(self.msg_config)
+        expected_url = (
             'https://api.nexmo.com/verify/json'
             '?api_key={api_key}'
             '&api_secret={api_secret}'
@@ -31,23 +28,18 @@ class VerificationMessageTest(TestCase):
                 api_secret=self.test_secret,
                 number=test_number,
                 brand=test_brand
-            )
-        )
+            ))
 
-        self.assertEqual(expected_request, request)
+        self.message_sent_test(sms, expected_url)
 
-    def test_it_should_build_a_verification_check_message(self):
-
+    def test_it_should_send_a_verification_check_message(self):
         request_id = '8g88g88eg8g8gg9g90'
         code = 123456
-        self.msg['request_id'] = request_id
-        self.msg['code'] = code
+        self.msg_config['request_id'] = request_id
+        self.msg_config['code'] = code
+        sms = NexmoVerificationCheck(self.msg_config)
 
-        sms = NexmoVerificationCheck(self.msg)
-
-        request = sms.build_request()
-
-        expected_request = (
+        expected_url = (
             'https://api.nexmo.com/verify/json'
             '?api_key={api_key}'
             '&api_secret={api_secret}'
@@ -60,8 +52,40 @@ class VerificationMessageTest(TestCase):
             )
         )
 
-        self.assertEqual(expected_request, request)
+        self.message_sent_test(sms, expected_url)
 
-    # def test_it_should_send_a_verification_message(self):
-    #     sms = NexmoVerification(self.msg)
+    def test_it_should_send_a_verification_status_message(self):
+        request_id = '8g88g88eg8g8gg9g90'
+        code = 123456
+        self.msg_config['request_id'] = request_id
+        self.msg_config['code'] = code
+        sms = NexmoVerificationCheck(self.msg_config)
 
+        expected_url = (
+            'https://api.nexmo.com/verify/json'
+            '?api_key={api_key}'
+            '&api_secret={api_secret}'
+            '&request_id={request_id}'
+            '&code={code}'.format(
+                api_key=self.test_key,
+                api_secret=self.test_secret,
+                request_id=request_id,
+                code=code
+            )
+        )
+
+        self.message_sent_test(sms, expected_url)
+
+    def message_sent_test(self, sms, expected_url):
+        with patch('nexmomessage.nexmo.urllib2') as urllib2:
+            request = Mock()
+            response = Mock()
+            response.code = 200
+            response.read.return_value = '{}'
+            urllib2.Request.return_value = request
+            urllib2.urlopen.return_value = response
+
+            sms.send_request()
+        urllib2.Request.assert_called_with(url=expected_url)
+        request.add_header.assert_called_with('Accept', 'application/json')
+        urllib2.urlopen.assert_called_with(request)
